3c152861977fb6a1f916a7d6eeb084b285882b98
Simone Taeggi <startae14@gmail.com>
2023-05-05 21:40:34 +0200

    connect React Component with redux and define a hook to dispatch action creator

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import { useActions } from "../hooks/useAction";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
+  const { searchRepositories } = useActions();
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    
+    searchRepositories(term);
   }
   return (
     <div>
